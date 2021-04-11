a817bd4ac92aa16f1f826e61ebcf539602b258e2
VaishnaviPunagin <va.punagin@gmail.com>
2021-04-11 18:11:03 +0530

    Update yacc_compile.h

--- yacc_compile.h
+++ yacc_compile.h
@@ -1,33 +1,30 @@
 typedef enum { typeCon, typeId, typeOpr } nodeEnum;
 
-typedef struct
-{	
-	int value; //constants
-}conNodeType;
+/* constants */
+typedef struct {
+    int value;
+} conNodeType;
 
-typedef struct
-{
-	int i; //identifiers
-}idNodeType;
+/* identifiers */
+typedef struct {
+    int i;
+} idNodeType;
 
-typedef struct
-{
-	//operators
-    	int oper;
-    	int nops;
-    	struct nodeTypeTag *op[1];
-}oprNodeType;
+/* operators */
+typedef struct {
+    int oper;
+    int nops;
+    struct nodeTypeTag *op[1];
+} oprNodeType;
 
-typedef struct nodeTypeTag
-{
-	nodeEnum type;
-    	union
-    	{
-        	conNodeType con;
-        	idNodeType id;
-        	oprNodeType opr;
-    	};
-}nodeType;
+typedef struct nodeTypeTag {
+    nodeEnum type;
+
+    union {
+        conNodeType con;
+        idNodeType id;
+        oprNodeType opr;
+    };
+} nodeType;
 
 extern int sym[26];
-
